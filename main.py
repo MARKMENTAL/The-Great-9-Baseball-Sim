288db19a0a64b58296cef0e5b038e470a6cb4c5e
MARKMENTAL <marky611@gmail.com>
2021-07-08 21:36:36 -0400

    Update main.py
    Added new teams and a menu bar

--- main.py
+++ main.py
@@ -11,35 +11,48 @@ def back(lbl1, combobox1):
     cputeam = ""
     humanteam = ""
     combovalue = combobox1["value"]
-    lbl1 = Label(window, text="", fg='red', font=("Cantarell", 12))
+    lbl1 = Label(window, text="", fg='green', font=("Cantarell", 12))
     if combobox1.get() != "":
-        cputeamrnd = random.randint(0, 2)
+        cputeamrnd = random.randint(0, 9)
         humanteamrnd = random.randint(0, 20)
         cputeam = combovalue[cputeamrnd]
-        cputeamscore = random.randint(0,20)
+        cputeamscore = random.randint(0, 20)
+        btnabt = Button(window, text="About", fg='navy')
+        btnabt.place(x=200, y=50)
+        btnabt.bind("<Button>", lambda e: about())
         if humanteamrnd == cputeamscore:
-            humanteamrnd += 1
+            humanteamrnd = humanteamrnd + 1
         lbl1["text"] = "Feel Free to Keep Simming!"
         messagebox.showinfo(title="Game Results", message=(str(combobox1.get()),  humanteamrnd, cputeam, cputeamscore))
+        if humanteamrnd > cputeamscore:
+            messagebox.showinfo(title="Game Results", message=("The ", str(combobox1.get()), " won! \n Good Job"))
+        lbl1.place(x=20, y=100)
+    if combobox1.get() == "":
+        lbl1["text"] = "Please Select a Team"
         lbl1.place(x=20, y=100)
 
 
 def front(window1):
+    menubar = Menu(window1)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Exit", command=window1.quit)
+    filemenu.add_command(label="About", command=about)
+    menubar.add_cascade(label="Options", menu=filemenu)
+    window1.config(menu=menubar)
     btn = Button(window1, text="Sim Game", fg='green')
-    btnabt = Button(window1, text="About", fg='navy')
-    stringthing = btn['text']
     lbl = Label(window1, text="Player 1 Select a team to simulate as \n"
                 " A random team will be the opponent ", fg='red', font=("Cantarell", 12))
-    titlelbl = Label(window1, text="The Great 9 Baseball Sim: Milestone 0", fg='red', font=("Cantarell", 12))
+    titlelbl = Label(window1, text="The Great 9 Baseball Sim: Milestone 1", fg='red', font=("Cantarell", 12))
     titlelbl.place(x=230, y=0)
     lbl.place(x=20, y=100)
     btn.place(x=70, y=50)
-    btnabt.place(x=200, y=50)
     combobox = ttk.Combobox(state="readonly")
-    combobox['value'] = ('Boston Boots', 'New York Americans', 'Seattle Sailors')
+    combobox['value'] = ('Boston Boots', 'New York Americans', 'Seattle Sailors'
+                         , "San Francisco Titans", "California Seraphims", "Buffalo Blue Birds", "Tokyo Tornadoes"
+                         , "Wyoming Westerners", "London Lookout", "Virginia Turkeys")
     combobox.place(x=50, y=150)
     btn.bind("<Button>", lambda e: back(lbl, combobox))
-    btnabt.bind("<Button>", lambda e: about())
+
     btn.pack
     window1.mainloop()
 
